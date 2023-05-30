43085f5c4464e595303d01fea1a8b1b21d702565
yanglixin <yanglixin@qiniu.com>
2023-05-30 17:16:07 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,21 +1,39 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
+  lang: "zh-CN",
+  
 
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
-  },
+  // locales: {
+  //   "/": {
+  //     lang: "en-US",
+  //     title: "Blog",
+  //   },
+  //   "/zh/": {
+  //     lang: "zh-CN",
+  //     title: "博客",
+  //   },
+  // },
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category as string,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag as string,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 
   theme,
 
